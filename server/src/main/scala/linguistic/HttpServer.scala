16fbc54e05f7668b310f2d3de9a11506757a13a3
Vadim Bondarev <haghard84@gmail.com>
2017-04-17 19:10:15 +0200

    debug

--- server/src/main/scala/linguistic/HttpServer.scala
+++ server/src/main/scala/linguistic/HttpServer.scala
@@ -1,18 +1,19 @@
 package linguistic
 
-import linguistic.Application._
-import linguistic.dao.UsersRepo
+import akka.actor.{Actor, ActorLogging, Props, Status}
 import akka.cluster.sharding.ClusterSharding
 import akka.stream.ActorMaterializerSettings
-import akka.actor.{Actor, ActorIdentity, ActorLogging, Identify, Props, Status}
-import linguistic.utils.ShutdownCoordinator
-import ShutdownCoordinator.NodeShutdownOpts
+import linguistic.Application._
 import linguistic.WordsSearchProtocol.{SearchHomophones, SearchWord}
+import linguistic.dao.UsersRepo
 import linguistic.dao.UsersRepo.Activate
 import linguistic.ps.{HomophonesSubTreeShardEntity, WordShardEntity}
+import linguistic.utils.ShutdownCoordinator
+import linguistic.utils.ShutdownCoordinator.NodeShutdownOpts
 
 object HttpServer {
   val HttpDispatcher = "akka.http.dispatcher"
+
   object Stop
 
   def props(port: Int, address: String, keypass: String, storepass: String) =
@@ -21,11 +22,12 @@ object HttpServer {
 
 class HttpServer(port: Int, address: String, keypass: String, storepass: String) extends Actor with ActorLogging
   with SslSupport with ShardingSupport {
+
   import HttpServer._
   import akka.http.scaladsl.Http
-  import akka.pattern.pipe
-  import akka.http.scaladsl.server.RouteResult._
   import akka.http.scaladsl.server.RouteConcatenation._
+  import akka.http.scaladsl.server.RouteResult._
+  import akka.pattern.pipe
 
   implicit val system = context.system
   implicit val ex = system.dispatchers.lookup(HttpDispatcher)
@@ -57,8 +59,9 @@ class HttpServer(port: Int, address: String, keypass: String, storepass: String)
 
   import scala.concurrent.Future
   import scala.concurrent.duration._
+
   def serverBinding(b: akka.http.scaladsl.Http.ServerBinding) = {
-    log.info("Binding on {}",  b.localAddress)
+    log.info("Binding on {}", b.localAddress)
 
     import akka.pattern.ask
 
@@ -68,7 +71,7 @@ class HttpServer(port: Int, address: String, keypass: String, storepass: String)
 
     Future.sequence(
       Seq((searchMaster ? SearchWord("average", 1)), (homophonesShard ? SearchHomophones("rose", 1))))
-      .onComplete {
+      .onComplete { _ =>
         users ! Activate
       }
 
@@ -85,6 +88,6 @@ class HttpServer(port: Int, address: String, keypass: String, storepass: String)
   def bound(b: akka.http.scaladsl.Http.ServerBinding): Receive = {
     case HttpServer.Stop =>
       log.info("Unbound {}:{}", address, port)
-      b.unbind().onComplete { _ =>  mat.shutdown }
+      b.unbind().onComplete { _ => mat.shutdown }
   }
 }
