4ae6fc0d2c2ba84da777888690bde51028c5643c
haghard <haghard84@gmail.com>
2018-08-20 11:55:37 +0300

    set nr-of-retries = 10

--- server/src/main/scala/linguistic/Application.scala
+++ server/src/main/scala/linguistic/Application.scala
@@ -50,7 +50,7 @@ object Application extends App with AppSupport {
        |  }
        |  max-nr-of-seed-nodes = 2
        |  coordination-timeout = 5 seconds
-       |  nr-of-retries        = 3
+       |  nr-of-retries        = 10
        |  join-timeout = 15 seconds
        |}
     """.stripMargin
